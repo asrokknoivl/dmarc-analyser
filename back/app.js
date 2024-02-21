7870161724238f44b2b5a9efdb5940e024a33016
asrokknoivl <mqaisahm@gmail.com>
2024-02-21 15:50:05 +0100

    update shit

--- back/app.js
+++ back/app.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 
 app.get(
     '/',
@@ -14,8 +25,10 @@ app.get(
 
 app.post(
     '/upload',
+    upload.single('file'),
     (req, res) => {
-        console.log(req)
+        console.log(req.file);
+        res.sendStatus(200);
     }
 )
 
